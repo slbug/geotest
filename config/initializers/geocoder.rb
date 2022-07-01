7ad36473ddb70cffffb3e1a75b3217d90d9f162f
Alexander Grebennik <aleks.grebennik@gmail.com>
2022-07-01 09:38:47 +0300

    ipstack

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,14 +1,13 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-#  ip_lookup: :maxmind,
-#  lookup: :ipstack,         # name of geocoding service (symbol)
-#  ip_lookup: :ipstack,          # name of IP address geocoding service (symbol)
+  lookup: :ipstack,         # name of geocoding service (symbol)
+  ip_lookup: :ipstack,          # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-#  api_key: ENV.fetch('GEOCODER_API_KEY'),               # API key for geocoding service
+  api_key: ENV.fetch('GEOCODER_API_KEY'),               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
 
   # Exceptions that should not be rescued by default
